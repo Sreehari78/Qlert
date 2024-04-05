cd701d4153ab6b5360851320fd7fafac3721b883
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2024-04-05 11:50:06 +0530

    Update global CSS colors and add Material Tailwind support

---
+++ client/src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+import { DefaultSidebar } from "@/components/Sidebar";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <DefaultSidebar />
+    </div>
+  );
+};
+
+export default page;
