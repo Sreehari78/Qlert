a257f72eb0a7a2be34398b26f4ba62dd7416f1cc
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2024-04-05 16:18:47 +0530

    Update Chatbox component with Tooltip and IconButton changes

--- client/src/components/Chatbox.tsx
+++ client/src/components/Chatbox.tsx
@@ -1,23 +1,35 @@
-import { Textarea, Button, IconButton } from "@material-tailwind/react";
+import {
+  Textarea,
+  Button,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
 
 export function ChatboxTextarea() {
   return (
     <div className='flex w-full flex-row items-center gap-2 rounded-[99px] border border-gray-400/10 bg-gray-400/5 p-2'>
       <div className='flex'>
-        <IconButton variant='text' className='rounded-full'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            stroke-width='2'
-            stroke-linecap='round'
-            stroke-linejoin='round'>
-            <path d='M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48'></path>
-          </svg>
-        </IconButton>
+        <Tooltip
+          content='Attach'
+          animate={{
+            mount: { scale: 1, y: 0 },
+            unmount: { scale: 0, y: 25 },
+          }}>
+          <IconButton variant='text' className='rounded-full'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='gray'
+              stroke-width='2'
+              stroke-linecap='round'
+              stroke-linejoin='round'>
+              <path d='M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48'></path>
+            </svg>
+          </IconButton>
+        </Tooltip>
       </div>
       <Textarea
         rows={1}
