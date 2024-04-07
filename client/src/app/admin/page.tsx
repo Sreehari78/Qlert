c8be616c8093f8044f9da479a2d8ce78baa42a5e
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2024-04-07 09:53:03 +0530

    Refactor API route to use new table name and remove unnecessary code

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -8,7 +8,7 @@ import { DataTable } from "@/components/Table";
 const page = () => {
   const [data, setData] = React.useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/api/hello")
+    fetch("http://localhost:3000/api/getTable")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
