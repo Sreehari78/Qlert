c1c82025d8278f16bf5e7685a119bfc40f112930
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-04-15 16:21:02 +0530

    Refactor Graph component to dynamically set the height based on window size

--- client/src/components/Graph.tsx
+++ client/src/components/Graph.tsx
@@ -10,9 +10,10 @@ export default function LineGraph(props: {
   const [windowHeight, setWindowHeight] = useState<number>(0);
 
   useEffect(() => {
-    if (windowHeight < 600) setWindowHeight(400);
-    else if (windowHeight >= 600 && windowHeight < 1024) setWindowHeight(300);
-    else setWindowHeight(425);
+    if (window.innerWidth < 350) setWindowHeight(300);
+    else if (window.innerWidth >= 350 && windowHeight < 750)
+      setWindowHeight(400);
+    else setWindowHeight(500);
   }, []);
 
   console.log(windowHeight);
@@ -105,7 +106,7 @@ export default function LineGraph(props: {
   };
   return (
     <Card>
-      <CardBody className='px-2 pb-0'>
+      <CardBody className="px-2 pb-0">
         <Chart {...chartConfig} />
       </CardBody>
     </Card>
