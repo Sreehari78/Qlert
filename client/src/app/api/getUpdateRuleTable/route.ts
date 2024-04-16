28f2a95927e33173388c95e0b9206a3316c141a8
Sreehari S <shraxx7@gmail.com>
2024-04-16 19:43:01 +0530

    Update route.ts

--- client/src/app/api/getUpdateRuleTable/route.ts
+++ client/src/app/api/getUpdateRuleTable/route.ts
@@ -1,8 +1,8 @@
 import { QdrantClient } from "@qdrant/js-client-rest";
 
 const client = new QdrantClient({
-  host: "e343766d-2df4-4ea9-aa13-000d153fdad6.us-east4-0.gcp.cloud.qdrant.io",
-  apiKey: "izwQ7y1tkXJ9OYh7EAbBW3evsmgsQmwNGkoe5HoZ9Hohb-ES1zyX8A",
+  host: "###",
+  apiKey: "###",
 });
 
 export async function GET(req: {}) {
